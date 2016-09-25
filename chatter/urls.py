a5a05de8492ccc13c8bcd8a6fe5942ccd95ca748
William <Wilche3@gmail.com>
2016-09-25 05:15:08 -0400

    Updates for heroku

--- chatter/urls.py
+++ chatter/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
     url(r'^chat/', include('chat.urls')),
     url(r'^admin/', admin.site.urls)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
