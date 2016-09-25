b8eb576435387725a490320888c3c7813c84ded2
Pramod Kotipalli <pramod.kotipalli@gmail.com>
2016-09-25 01:32:44 -0400

    Added events for force-update and video-end

--- examples/chatter/chat/routing.py
+++ examples/chatter/chat/routing.py
@@ -9,7 +9,10 @@ event_routing = [
     route('chat.receive', events.user_join, event=r'^user-join$'),
     route('chat.receive', events.user_leave, event=r'^user-leave$'),
     route('chat.receive', events.state_change, event=r'^state-change$'),
-    route('chat.receive', events.client_send, event=r'^message-send$')
+    route('chat.receive', events.client_send, event=r'^message-send$'),
+
+    route('chat.receive', events.video_end, event=r'^video-end$'),
+    route('chat.receive', events.force_update, event=r'^force-update$'),
 ]
 
 binding_routing = [
